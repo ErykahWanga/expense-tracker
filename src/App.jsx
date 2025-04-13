87b06415b2cddca49da43dfe95fb6838f534dcea
Erykah <erkahwanga@gmail.com>
2025-04-13 18:13:53 +0300

    Add basic App.jsx component with title

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseTable from './components/ExpenseTable';
+import SearchBar from './components/SearchBar';
+import './App.css';
+
